ac987598e37f17dabd00eff7b750f5a840382d84
Oisiky <oisiky.sinha.2007@gmail.com>
2026-02-28 22:54:48 +0530

    work in progress

---
+++ ml_project2.py
@@ -0,0 +1,8 @@
+'''                                         AI Text Generator - Next Word Predictor
+a probability machine which lerns the pattern based on the previous words and predicts the most likely next word.'''
+
+#LSTM = Long-Term Short Memory; gets rid of the vanishing gradient problem in Recurrent Neural Networks.
+#n-gram sequence creation => using one case as multiple training sample.
+
+#working with Marry Shelly's "Frankenstein or The Modern Prometheus"; downloaded from 'The Project Gutenberg'
+
